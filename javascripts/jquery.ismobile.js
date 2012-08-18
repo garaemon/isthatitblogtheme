ca2081eb8a2440231412728e99bce0861ff73706
garaemon <garaemon@gmail.com>
2012-08-18 17:11:34 +0900

    supporting contenthover

---
+++ javascripts/jquery.ismobile.js
@@ -0,0 +1,23 @@
+// http://www.abeautifulsite.net/blog/2011/11/detecting-mobile-devices-with-javascript/
+(function($) {
+  var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i) ? true : false;
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) ? true : false;
+    },
+    any: function() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+  };
+  $.isMobile = function() {
+    return isMobile.any();
+  };
+})(jQuery);
--- javascripts/kicksontheroad.js
+++ javascripts/kicksontheroad.js
@@ -1,4 +1,39 @@
 // kicksontheroad.js
+// title
+$(function() {
+  $("#title-wrapper .fb-like")
+    .css("left", screen.width / 2 + 285)
+    .css("top", 60);
+});
 
 $(function() {
+  // util
+  function startsWith(str, prefix) {
+    return str.indexOf(prefix) === 0;
+  };
+  function endsWith(str, suffix) {
+    return str.match(suffix+"$") == suffix;
+  };
+  
+  $(".snapbox").each(function() {
+    var $snapbox = $(this);
+    $(this).find("img").imagesLoaded(function() {
+      var name = "";
+      $snapbox.find(".tag").each(function() { //parse names
+        var $a = $(this).find("a");
+        var tag = $a.html();
+        if (startsWith(tag, "name:")) {
+          name = tag.slice(5);
+        }
+      });
+      var $contenthover = $('<div class="contenthover center">'
+                            + name.toUpperCase() + '</div>');
+      $contenthover.css("height", $(this).height());
+      $contenthover.css("line-height", $(this).height() + "px");
+      $(this).after($contenthover);
+      $(this).contenthover({
+        effect: "show"
+      });
+    });
+  });
 });
