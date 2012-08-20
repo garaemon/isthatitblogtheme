e91df2e7609be6145ea1c3cd8fff1c4d9865e100
garaemon <garaemon@gmail.com>
2012-08-20 11:17:58 +0900

    adding more debug messages

--- javascripts/kicksontheroad.js
+++ javascripts/kicksontheroad.js
@@ -79,6 +79,7 @@ $(function() {
         //$("#container").append($snapboxes);
         loaded_index = loaded_index + 1;
         $snapboxes.imagesLoaded(function() {
+          console.log("images are loaded at " + loaded_index);
           bottom_lock = true;
         });
       }
