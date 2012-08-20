12166f8ab80243caa54bbe8d65f7d066d89946de
garaemon <garaemon@gmail.com>
2012-08-20 11:39:02 +0900

    adding masonry

--- javascripts/kicksontheroad.js
+++ javascripts/kicksontheroad.js
@@ -43,7 +43,7 @@ $(function() {
       $(this).contenthover({
         effect: "show"
       });
-      //$("#container").masonry("appended", $snapbox);
+      $("#container").masonry("appended", $snapbox);
     });
  };
   
